47cf9c99b0578d82a715252da87bd4bb21a9c14b
yahmiamel <44164088+yahmiamel@users.noreply.github.com>
2018-11-05 13:53:53 +0100

    Add files via upload

---
+++ tp2/formulaire.js
@@ -0,0 +1,49 @@
+function validation ()
+{
+
+
+
+if (document.getElementById("Nom").value==""||document.getElementById("Nom").value.length<5) {
+
+
+
+    document.getElementById("error").innerHTML = "La saisie du nom est obligatoire";
+    
+} else {     document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value;
+
+}
+
+{if ( document.getElementById("Prenom").value==""||document.getElementById("Prenom").value.length<5) 
+
+    document.getElementById("error").innerHTML = "La saisie du Prenom est obligatoire";
+
+
+    else {     document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value;
+
+
+    }
+        if (document.getElementById("date").value==""||document.getElementById("date").value.length<5) {
+        document.getElementById("error").innerHTML = "La saisie de la date est obligatoire";
+
+        
+    } else {     document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value;
+
+}
+        if (document.getElementById("Adresse").value==""||document.getElementById("Adresse").value.length<5) {
+            document.getElementById("error").innerHTML = "La saisie d'adresse est obligatoire";
+            
+        } else {     document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value;
+
+    }
+            if (document.getElementById("email").value==""||document.getElementById("email").value.length<5) {
+
+                document.getElementById("error").innerHTML = "La saisie d'email est obligatoire";
+                
+            } else{    document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value;
+
+        }
+        }
+    }
+
+
+
